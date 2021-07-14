759975317fa4077d5dd9cd9bb75fcbc5d39b70a4
ssda332 <tititiqkfk3@gmail.com>
2021-07-14 10:27:50 +0900

    환경설정

---
+++ src/main/java/com/yj/community/CommunityApplication.java
@@ -0,0 +1,13 @@
+package com.yj.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
